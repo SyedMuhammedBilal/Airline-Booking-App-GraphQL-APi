8649f84db1ed62aa71c0d867fe77f761cf1b6c62
Syed Muhammed Bilal <intergo878@gmail.com>
2021-06-25 18:39:55 +0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: "mongodb+srv://syed_bilal:1234@cluster0.wwm7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    SECRET_KEY: "HARMONYSONEXRPADAATAICPPOLS"
-}
